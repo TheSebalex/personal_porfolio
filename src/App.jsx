1c9172c7389271abc7321167820e76974ed974cf
Sebastian Leañez <thesebalex111@gmail.com>
2024-02-08 02:21:37 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { Route, HashRouter, Routes } from "react-router-dom";
-import { Layout } from "./Components/Layout";
-import { Home } from "./Components/Routes/Home/Home"
-import { Experience } from "./Components/Routes/Experience/Experience";
-import { Portfolio } from "./Components/Routes/Portfolio/Portfolio";
-import { Contact } from "./Components/Routes/Contact/Contact";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// ... tus otros imports ...
 
 function App() {
   return (
     <>
-      <HashRouter>
+      <Router basename={process.env.NODE_ENV === 'production' ? '/personal_porfolio' : '/'}>
         <Routes>
             <Route path="/" element={<Layout children={<Home/>}/>}></Route>
             <Route path="/experience" element={<Layout children={<Experience/>}/>}></Route>
             <Route path="/portfolio" element={<Layout children={<Portfolio/>}/>}></Route>
             <Route path="/contact" element={<Layout children={<Contact/>}/>}></Route>
         </Routes>
-      </HashRouter>
+      </Router>
     </>
   );
 }
