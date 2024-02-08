9ddfbd8238cf283157914c9448e3cc564b69deff
Sebastian Leañez <thesebalex111@gmail.com>
2024-02-08 02:16:06 -0400

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import { Layout } from "./Components/Layout";
 import { Home } from "./Components/Routes/Home/Home"
 import { Experience } from "./Components/Routes/Experience/Experience";
@@ -8,14 +8,14 @@ import { Contact } from "./Components/Routes/Contact/Contact";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
             <Route path="/" element={<Layout children={<Home/>}/>}></Route>
             <Route path="/experience" element={<Layout children={<Experience/>}/>}></Route>
             <Route path="/portfolio" element={<Layout children={<Portfolio/>}/>}></Route>
             <Route path="/contact" element={<Layout children={<Contact/>}/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
