476d3789fa75a5ef1df6e52462f36dd121b733e0
Sebastian Leañez <thesebalex111@gmail.com>
2024-02-15 13:48:48 -0400

    images

--- src/Components/Routes/Portfolio/Projects.js
+++ src/Components/Routes/Portfolio/Projects.js
@@ -1,11 +1,26 @@
 export const projects = [
-    {
-        title: "SWC",
-        imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
-        desc:"Este proyecto es una landing page de una comunidad deportiva.",
-        tech: "Javascript || CSS || HTML",
-        gallery: ["https://i.postimg.cc/yYN6y15w/image.png"],
-        link:"https://thesebalex.github.io/SWC/"
-    },
-
-]
+  {
+    title: "SWC",
+    imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
+    desc: "Este proyecto es una landing page de una comunidad deportiva.",
+    tech: "Javascript || CSS || HTML",
+    gallery: ["https://i.postimg.cc/yYN6y15w/image.png"],
+    link: "https://thesebalex.github.io/SWC/",
+  },
+  {
+    title: "LoverGym",
+    imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
+    desc: "Proyecto de enfoque social, que junta app de rutinas y una red social interactiva en una sola solucion web.",
+    tech: "React || Javascript || Tailwind || NPM || GIT || JSX",
+    gallery: ["https://i.postimg.cc/43ypsv2w/LOVERGYM.png", "https://i.postimg.cc/XYQ9R1Lp/LOVERGYM-2.png",
+    "https://i.postimg.cc/d0fr17zF/LOVERGYM-3.png "],
+    link: "https://lovergym.com/",
+  },
+  {
+    title: "Persant",
+    imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
+    desc: "Esta es una app web para la gestión de gastos personales, la cual permite registrar gastos y categorizarlos, asi como llevar un control de cuentas contables, enfocado a como llevaria la contabilidad una empresa, pero a un nivel micro - personal.",
+    tech: "Node.js || Express.js || SQL Server || React || JSX || Tailwind || NPM || GIT",
+    gallery: ["https://i.postimg.cc/d3JwF3Hm/PERSANT.png", "https://i.postimg.cc/dVfsCq3w/PERSANT-2.png", "https://i.postimg.cc/mDfTd87n/PERSANT-3.png", "https://i.postimg.cc/MGjWbhfR/PERSANT-4.png"]
+  },
+];
