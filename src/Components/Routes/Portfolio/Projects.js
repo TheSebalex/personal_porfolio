fcd2816899b8116b8bf2a7b62f6e506f6e176a38
Sebastian Leañez <thesebalex111@gmail.com>
2024-02-15 13:51:11 -0400

    images titles

--- src/Components/Routes/Portfolio/Projects.js
+++ src/Components/Routes/Portfolio/Projects.js
@@ -9,18 +9,18 @@ export const projects = [
   },
   {
     title: "LoverGym",
-    imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
+    imageTitle: "https://i.postimg.cc/43ypsv2w/LOVERGYM.png",
     desc: "Proyecto de enfoque social, que junta app de rutinas y una red social interactiva en una sola solucion web.",
     tech: "React || Javascript || Tailwind || NPM || GIT || JSX",
-    gallery: ["https://i.postimg.cc/43ypsv2w/LOVERGYM.png", "https://i.postimg.cc/XYQ9R1Lp/LOVERGYM-2.png",
+    gallery: [ "https://i.postimg.cc/XYQ9R1Lp/LOVERGYM-2.png",
     "https://i.postimg.cc/d0fr17zF/LOVERGYM-3.png "],
     link: "https://lovergym.com/",
   },
   {
     title: "Persant",
-    imageTitle: "https://i.postimg.cc/HnqpLdLx/image.png",
+    imageTitle: "https://i.postimg.cc/d3JwF3Hm/PERSANT.png",
     desc: "Esta es una app web para la gestión de gastos personales, la cual permite registrar gastos y categorizarlos, asi como llevar un control de cuentas contables, enfocado a como llevaria la contabilidad una empresa, pero a un nivel micro - personal.",
     tech: "Node.js || Express.js || SQL Server || React || JSX || Tailwind || NPM || GIT",
-    gallery: ["https://i.postimg.cc/d3JwF3Hm/PERSANT.png", "https://i.postimg.cc/dVfsCq3w/PERSANT-2.png", "https://i.postimg.cc/mDfTd87n/PERSANT-3.png", "https://i.postimg.cc/MGjWbhfR/PERSANT-4.png"]
+    gallery: ["https://i.postimg.cc/dVfsCq3w/PERSANT-2.png", "https://i.postimg.cc/mDfTd87n/PERSANT-3.png", "https://i.postimg.cc/MGjWbhfR/PERSANT-4.png"]
   },
 ];
